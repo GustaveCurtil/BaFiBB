b2a8c8e1530c388a4ca536dd348006f98e7d86ea
Gustave Curtil <gustave.curtil@tutanota.com>
2024-08-01 13:22:48 +0200

    meer rugzakken plies

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ function shuffleArray(array) {
 
 shuffleArray(images);
 
-let totalDisplayed = 5;
+let totalDisplayed = 15;
 
 // Ensure totalDisplayed does not exceed the number of available images
 
